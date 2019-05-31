a46cd33d05fda79849848743ce51749ea2f3ff71
oGsLP <1145234011@qq.com>
2019-05-31 17:49:04 +0800

    modify main entry

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,50 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <type-writer :interval=50>
-      <span class="comments">/**</span><br />
-      <span class="comments">*2018—04-23,</span
-      ><br />
-      <span class="comments">*2019-05-20.</span
-      ><br />
-      <span class="comments">*/</span><br />
-      <span class="keyword">const</span><span> boy =</span
-    ><span class="highlight">Xiao Yunfan</span><br />
-      <span class="keyword">const</span><span> girl =</span
-    ><span class="highlight">Sun Yu</span><br />
-      <br />
+	<div id="app">
+		<type-writer :interval="50">
+			<span class="comments">/**</span><br />
+			<span class="comments">*2018—04-23,</span><br />
+			<span class="comments">*2019-05-20.</span><br />
+			<span class="comments">*/</span><br />
+			<span class="keyword">const</span><span> boy =</span
+			><span class="highlight">Xiao Yunfan</span><br />
+			<span class="keyword">const</span><span> girl =</span
+			><span class="highlight">Sun Yu</span><br />
+			<br />
 
-      <span>They tried to knew each other gradually,</span><br />
-      <span>And fell in love quickly.</span><br />
-      <br />
-    </type-writer>
-  </div>
+			<span>They tried to knew each other gradually,</span><br />
+			<span>And fell in love quickly.</span><br />
+			<br />
+		</type-writer>
+	</div>
 </template>
 
 <script>
-import TypeWriter from './components/TypeWriter.vue'
+import TypeWriter from "./components/TypeWriter.vue";
 
 export default {
-  name: 'app',
-  components: {
-    TypeWriter
-  }
-}
+	name: "app",
+	components: {
+		TypeWriter
+	}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: "Avenir", Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+.comments {
+	color: green;
+}
+.keyword {
+	color: rebeccapurple;
+}
+.highlight {
+	color: hotpink;
 }
 </style>
--- src/components/TypeWriter.vue
+++ src/components/TypeWriter.vue
@@ -1,82 +1,91 @@
 <template>
-    <div class="v-typewriter">
-        <slot></slot>
-    </div>
+	<div
+		class="v-typewriter"
+		:style="{
+			textAlign: align
+		}"
+	>
+		<slot></slot>
+	</div>
 </template>
 
 <script>
-    export default {
-        name: "TypeWriter",
-        props: {
-            order:{
-              type: String,
-              default: "normal"
-            },
-            oneTime: {
-                type: Boolean,
-                default: true
-            },
-            delay:{
-                type: Number,
-                default: 0
-            },
-            interval: {
-                type: Number,
-                default: 75
-            },
-            cursor:{
-                type:[Boolean, String],
-                default: "_"
-            }
-        },
-        data(){
-           return{
-               ifCycle: this.oneTime,
-           };
-        },
-        created(){
-
-        },
-        mounted() {
-            this.typewrite(this.delay,this.interval,this.cursor);
-        },
-        computed:{
-
-        },
-        methods:{
-            typewrite(delay,interval,cursor){
-                if(delay===0)
-                    this.typeNoDelay(interval,cursor);
-                else
-                    setTimeout(this.typeNoDelay(interval,cursor),delay);
-            },
-            typeNoDelay(interval,cursor){
-                let html = document.querySelector(".v-typewriter");
-                let content = html.innerHTML;
-                html.innerHTML="";
-                let progress=0;
-                let timer = setInterval(function () {
-
-                    let current = content.substr(progress, 1);
-                    if (current === "<") {
-                        progress = content.indexOf(">", progress) + 1;
-                    } else {
-                        progress++;
-                    }
-                    html.innerHTML=(content.substring(0, progress) + (progress & 1 ? cursor : ""));
-                    if (progress >= content.length) {
-                        clearInterval(timer);
-                    }
-                },interval);
-            },
-            // typeText(node){
-            //     if(node.hasChildNodes()){
-            //     }
-            // }
-        }
-    }
+export default {
+	name: "TypeWriter",
+	props: {
+		order: {
+			type: String,
+			default: "normal"
+		},
+		oneTime: {
+			type: Boolean,
+			default: true
+		},
+		delay: {
+			type: Number,
+			default: 0
+		},
+		interval: {
+			type: Number,
+			default: 75
+		},
+		cursor: {
+			type: [Boolean, String],
+			default: "_"
+		},
+		align: {
+			type: String,
+			default: "left"
+		}
+	},
+	data() {
+		return {
+			ifCycle: this.oneTime
+		};
+	},
+	created() {},
+	mounted() {
+		this.typewrite();
+	},
+	computed: {},
+	methods: {
+		typewrite() {
+			if (this.delay === 0) this.typeNoDelay(this.interval, this.cursor);
+			else
+				setTimeout(
+					this.typeNoDelay(this.interval, this.cursor),
+					this.delay
+				);
+		},
+		typeNoDelay(interval, cursor) {
+			let html = document.querySelector(".v-typewriter");
+			let content = html.innerHTML;
+			html.innerHTML = "";
+			let progress = 0;
+			let timer = setInterval(function() {
+				let current = content.substr(progress, 1);
+				if (current === "<") {
+					progress = content.indexOf(">", progress) + 1;
+				} else {
+					progress++;
+				}
+				html.innerHTML =
+					content.substring(0, progress) +
+					(progress & 1 ? cursor : "");
+				if (progress >= content.length) {
+					clearInterval(timer);
+				}
+			}, interval);
+		}
+	}
+};
 </script>
 
 <style scoped>
-
+.v-typewriter {
+	margin: 5px;
+	width: 100%;
+	height: auto;
+	text-align: left;
+}
 </style>
